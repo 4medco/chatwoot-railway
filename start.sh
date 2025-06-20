4ca5c2096dba02587e42c44fc30c44411f615292
Samuel <4medco.holding@gmail.com>
2025-06-19 22:12:01 -0300

    Remove static storage.yml and dynamically generate it in start.sh if it doesn't exist

--- start.sh
+++ start.sh
@@ -2,6 +2,25 @@
 
 set -e
 
+# Gera config/storage.yml dinamicamente se não existir
+if [ ! -f config/storage.yml ]; then
+  mkdir -p config
+  cat <<EOF > config/storage.yml
+s3:
+  service: S3
+  access_key_id: <%= ENV['AWS_ACCESS_KEY_ID'] %>
+  secret_access_key: <%= ENV['AWS_SECRET_ACCESS_KEY'] %>
+  region: <%= ENV['AWS_REGION'] %>
+  bucket: <%= ENV['S3_BUCKET_NAME'] %>
+  endpoint: <%= ENV['S3_ENDPOINT'] %>
+  force_path_style: true
+
+local:
+  service: Disk
+  root: <%= Rails.root.join("storage") %>
+EOF
+fi
+
 echo Waiting for database...
 
 while ! pg_isready -h ${PGHOST} -p ${PGPORT}; do sleep 0.25; done; 
